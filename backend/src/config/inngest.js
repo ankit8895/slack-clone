9564e37298f219bf3d2bc41d736139c24fe00561
ankit8895 <ankit9185731@gmail.com>
2026-04-20 13:04:41 +0530

    fix issue of clerkMiddleware()

--- backend/src/config/inngest.js
+++ backend/src/config/inngest.js
@@ -6,8 +6,7 @@ export const inngest = new Inngest({ id: "slack-clone" });
 
 // to create/sync user from inngest to mongoDB
 const syncUser = inngest.createFunction(
-  { id: "sync-user" },
-  { event: "clerk/user.created" },
+  { id: "sync-user", triggers: [{ event: "clerk/user.created" }] },
   async ({ event }) => {
     await connectDB();
     const { id, first_name, last_name, image_url, email_addresses } =
@@ -25,8 +24,7 @@ const syncUser = inngest.createFunction(
 );
 
 const deleteUserFromDB = inngest.createFunction(
-  { id: "delete-user-from-db" },
-  { event: "clerk/user.deleted" },
+  { id: "delete-user-from-db", triggers: [{ event: "clerk/user.deleted" }] },
   async ({ event }) => {
     await connectDB();
     const { id } = event.data;
